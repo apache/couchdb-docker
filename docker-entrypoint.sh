33a1efc0db4449247b0b92b67c73768e67107277
klaemo <clemens.stolle@gmail.com>
2014-11-20 21:07:47 +0100

    apparently we need to manually set HOME with gosu
    - https://github.com/tianon/gosu/issues/3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,7 +17,7 @@ if [ "$1" = 'couchdb' ]; then
 
   chmod 664 /usr/local/etc/couchdb/*.ini
   chmod 775 /usr/local/etc/couchdb/*.d
-  exec gosu couchdb "$@"
+  HOME=/var/lib/couchdb exec gosu couchdb "$@"
 fi
 
 exec "$@"
