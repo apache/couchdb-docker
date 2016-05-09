29ed69965ed616a9d0df9a6ffa081773d86c78bc
Clemens Stolle <klaemo@fastmail.fm>
2016-05-09 19:54:09 +0200

    improve admin-party detection (#53)

--- 1.6.1/docker-entrypoint.sh
+++ 1.6.1/docker-entrypoint.sh
@@ -14,35 +14,6 @@
 set -e
 
 if [ "$1" = 'couchdb' ]; then
-
-	if [ "$COUCHDB_USER" ] && [ "$COUCHDB_PASSWORD" ]; then
-		# Create admin
-		printf "[admins]\n$COUCHDB_USER = $COUCHDB_PASSWORD\n" > /usr/local/etc/couchdb/local.d/docker.ini
-	fi
-
-	# Start CouchDB
-	couchdb -b > /dev/null 2>&1
-	while ! curl -s 127.0.0.1:5984 > /dev/null; do sleep 1; done
-
-	if [ "$(curl -s 127.0.0.1:5984/_config/admins)" == "{}" ]; then
-		# The - option suppresses leading tabs but *not* spaces. :)
-		cat >&2 <<-'EOWARN'
-			****************************************************
-			WARNING: CouchDB is running in Admin Party mode.
-			         This will allow anyone with access to the
-			         CouchDB port to access your database. In
-			         Docker's default configuration, this is
-			         effectively any other container on the same
-			         system.
-			         Use "-e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password"
-			         to set it in "docker run".
-			****************************************************
-		EOWARN
-	fi
-
-	# Stop CouchDB
-	couchdb -d > /dev/null 2>&1
-
 	# we need to set the permissions here because docker mounts volumes as root
 	chown -R couchdb:couchdb \
 		/usr/local/var/lib/couchdb \
@@ -58,6 +29,29 @@ if [ "$1" = 'couchdb' ]; then
 
 	chmod 664 /usr/local/etc/couchdb/*.ini
 	chmod 775 /usr/local/etc/couchdb/*.d
+
+	if [ "$COUCHDB_USER" ] && [ "$COUCHDB_PASSWORD" ]; then
+		# Create admin
+		printf "[admins]\n$COUCHDB_USER = $COUCHDB_PASSWORD\n" > /usr/local/etc/couchdb/local.d/docker.ini
+		chown couchdb:couchdb /usr/local/etc/couchdb/local.d/docker.ini
+	fi
+
+	# if we don't find an [admins] section followed by a non-comment, display a warning
+	if ! grep -Pzoqr '\[admins\]\n[^;]\w+' /usr/local/etc/couchdb; then
+		# The - option suppresses leading tabs but *not* spaces. :)
+		cat >&2 <<-'EOWARN'
+			****************************************************
+			WARNING: CouchDB is running in Admin Party mode.
+			         This will allow anyone with access to the
+			         CouchDB port to access your database. In
+			         Docker's default configuration, this is
+			         effectively any other container on the same
+			         system.
+			         Use "-e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password"
+			         to set it in "docker run".
+			****************************************************
+		EOWARN
+	fi
 
 	exec gosu couchdb "$@"
 fi
